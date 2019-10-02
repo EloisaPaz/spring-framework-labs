3eb3eada29f79df5672881286e2674645c4acb5c
EloisaPaz <eloisapaz28@gmail.com>
2019-10-02 15:11:30 -0300

    Adicionando user microservice Dto

---
+++ photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/service/UserServie.java
@@ -0,0 +1,8 @@
+package com.eloisapaz.photoapp.api.users.service;
+
+import com.eloisapaz.photoapp.api.users.shared.UserDto;
+
+public interface UserServie {
+	
+	UserDto createUser(UserDto userDetails);
+}
---
+++ photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.eloisapaz.photoapp.api.users.service.impl;
+
+import com.eloisapaz.photoapp.api.users.shared.UserDto;
+import com.eloisapaz.photoapp.api.users.service.UserServie;
+
+public class UserServiceImpl implements UserServie {
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
