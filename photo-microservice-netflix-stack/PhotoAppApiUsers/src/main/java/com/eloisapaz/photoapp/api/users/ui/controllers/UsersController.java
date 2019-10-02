666ca07062ec11b03d49e2f359371ca584f42f2f
EloisaPaz <eloisapaz28@gmail.com>
2019-10-02 14:56:15 -0300

    Adicionando H2 no user microservice e criando metodo post no controller

--- photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/ui/controllers/UsersController.java
+++ photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,10 +1,16 @@
 package com.eloisapaz.photoapp.api.users.ui.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eloisapaz.photoapp.api.users.ui.model.request.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -16,6 +22,11 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port: " + environment.getProperty("local.server.port");
+	}
+	
+	@PostMapping
+	public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		return "Create user is called!";
 	}
 
 }
