f61959b554dbe839d477c0df199c7d65b9057cd6
EloisaPaz <eloisapaz28@gmail.com>
2019-10-02 15:36:11 -0300

    Adicionando user entity para JPA

--- photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/service/impl/UserServiceImpl.java
+++ photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/service/impl/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.eloisapaz.photoapp.api.users.service.impl;
 
 import com.eloisapaz.photoapp.api.users.shared.UserDto;
+
+import java.util.UUID;
+
 import com.eloisapaz.photoapp.api.users.service.UserServie;
 
 public class UserServiceImpl implements UserServie {
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
-		// TODO Auto-generated method stub
+		userDetails.setUserId(UUID.randomUUID().toString());
 		return null;
 	}
 
