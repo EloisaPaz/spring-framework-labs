55b09fa5b0932c02ad77ff07777d3ebc08555f67
EloisaPaz <eloisapaz28@gmail.com>
2019-10-07 15:49:27 -0300

    Não funciona, problema com acesso negado

--- photo-microservice-netflix-stack/PhotoAppZuulApiGateway/src/main/java/com/eloisapaz/photoapp/api/gateway/security/WebSecurity.java
+++ photo-microservice-netflix-stack/PhotoAppZuulApiGateway/src/main/java/com/eloisapaz/photoapp/api/gateway/security/WebSecurity.java
@@ -22,13 +22,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
-		http.authorizeRequests()
-		.antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
-		.antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
-		.antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
-		.anyRequest().authenticated();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.csrf().disable(); 
+    	http.headers().frameOptions().disable();
+    	http.authorizeRequests()
+    	.antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
+    	.antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+    	.antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+    	.anyRequest().authenticated()
+    	.and()
+    	.addFilter(new AuthorizationFilter(authenticationManager(), environment));
+
+    	http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 }
