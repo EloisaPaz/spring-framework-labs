b5601a5ed6e174d41ebc9e459a6937ee187c8338
EloisaPaz <eloisapaz28@gmail.com>
2019-10-07 13:19:38 -0300

    Implementando metodo successfulAuthentication

--- photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/security/AuthenticationFilter.java
+++ photo-microservice-netflix-stack/PhotoAppApiUsers/src/main/java/com/eloisapaz/photoapp/api/users/security/AuthenticationFilter.java
@@ -2,18 +2,39 @@ package com.eloisapaz.photoapp.api.users.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.eloisapaz.photoapp.api.users.service.UserService;
+import com.eloisapaz.photoapp.api.users.shared.UserDto;
 import com.eloisapaz.photoapp.api.users.ui.model.request.LoginRequestModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+	
+	private UserService userService;
+	private Environment environment;
+	
+	public AuthenticationFilter(UserService userService, Environment environment, AuthenticationManager authenticationManager) {
+		this.userService = userService;
+		this.environment = environment;
+		super.setAuthenticationManager(authenticationManager);
+	}
 	
 	@Override
     public Authentication attemptAuthentication(HttpServletRequest request,
@@ -37,5 +58,16 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 	@Override
 	public void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
 		
+		String userName = ((User) authentication.getPrincipal()).getUsername();
+		UserDto userDetails = userService.getUserDetailsByEmail(userName);
+		
+		String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
+                .signWith(SignatureAlgorithm.HS512, environment.getProperty("token.secret") )
+                .compact();
+		
+		response.addHeader("token", token);
+		response.addHeader("userId", userDetails.getUserId());
 	}
 }
