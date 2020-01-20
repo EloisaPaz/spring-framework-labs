21a8f16c6ec6165748c7fc61f69170c931932407
Eloisa Paz <eloisapaz28@gmail.com>
2020-01-20 18:58:49 -0300

    spring boot test course lesson 1

---
+++ spring-tests/com.eloisapaz.money/src/main/java/com/eloisapaz/money/Dollar.java
@@ -0,0 +1,15 @@
+package com.eloisapaz.money;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times( int multiplier) {
+        amount *= multiplier;
+    }
+
+}
