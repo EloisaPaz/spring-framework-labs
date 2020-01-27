333fa23df3165b2b169a1964d60541fd81c90d70
EloisaPaz <eloisapaz28@gmail.com>
2020-01-27 15:02:27 -0300

    Add rate method on bank

--- spring-tests/com.eloisapaz.money/src/main/java/com/eloisapaz/money/Bank.java
+++ spring-tests/com.eloisapaz.money/src/main/java/com/eloisapaz/money/Bank.java
@@ -5,4 +5,8 @@ public class Bank {
     Money reduce(Expression source, String toCurrency) {
         return source.reduce(toCurrency);
     }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
 }
