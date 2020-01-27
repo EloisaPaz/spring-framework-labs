7eb76fd5142eabc5bf89c73dd781239a4c260640
EloisaPaz <eloisapaz28@gmail.com>
2020-01-27 11:24:41 -0300

    Add bank and expression

---
+++ spring-tests/com.eloisapaz.money/src/main/java/com/eloisapaz/money/Expression.java
@@ -0,0 +1,4 @@
+package com.eloisapaz.money;
+
+public interface Expression {
+}
--- spring-tests/com.eloisapaz.money/src/test/java/com/eloisapaz/money/MoneyTest.java
+++ spring-tests/com.eloisapaz.money/src/test/java/com/eloisapaz/money/MoneyTest.java
@@ -30,11 +30,21 @@ class MoneyTest {
 		assertNotEquals(Money.franc(5), Money.franc(8));
 		assertNotEquals(Money.franc(5), Money.dollar(5));
 	}
-	
+
 
 	@Test
 	void testCurrency() {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
+	}
+
+	@Test
+	void testSimpleAddition() {
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(10), reduced);
+
 	}
 }
