8a84e07f72472d2fe82d89fbc898d1439022bd55
EloisaPaz <eloisapaz28@gmail.com>
2020-01-27 15:21:27 -0300

    Refactor bank class

--- spring-tests/com.eloisapaz.money/src/main/java/com/eloisapaz/money/Bank.java
+++ spring-tests/com.eloisapaz.money/src/main/java/com/eloisapaz/money/Bank.java
@@ -1,15 +1,24 @@
 package com.eloisapaz.money;
 
+import java.util.HashMap;
+
 public class Bank {
+
+    private HashMap<Pair, Integer> rateMap = new HashMap<>();
 
     Money reduce(Expression source, String toCurrency) {
         return source.reduce(this, toCurrency);
     }
 
     public int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if(from.equals(to)) {
+            return 1;
+        }
+
+       return rateMap.get(new Pair(from, to));
     }
 
     public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from, to), rate);
     }
 }
